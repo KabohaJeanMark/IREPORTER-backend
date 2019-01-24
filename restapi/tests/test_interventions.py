7f2d07d755d03c29c3eded7a87117e634005bdad
Jean Mark <kabohajeanmark@gmail.com>
2019-01-24 23:37:23 +0300

    Add test for get a specific users interventions

--- restapi/tests/test_interventions.py
+++ restapi/tests/test_interventions.py
@@ -14,7 +14,7 @@ class TestIntervention(TestDb):
         }
         res = self.app.post(
             '/api/v1/interventions', content_type='application/json',
-             headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
+            headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
         self.assertEqual(res.status_code, 201)
         response = json.loads(res.data.decode())
         self.assertEqual(response['status'], 201)
@@ -43,11 +43,11 @@ class TestIntervention(TestDb):
         }
         res = self.app.post(
             '/api/v1/interventions', content_type='application/json',
-             headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
+            headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
         self.assertEqual(res.status_code, 201)
         response = self.app.get(
             '/api/v1/interventions/1', content_type='application/json',
-             headers={'Authorization': f'Bearer {token}'})
+            headers={'Authorization': f'Bearer {token}'})
 
         self.assertEqual(response.status_code, 200)
 
@@ -61,9 +61,26 @@ class TestIntervention(TestDb):
         }
         res = self.app.post(
             '/api/v1/interventions', content_type='application/json',
-             headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
+            headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
         self.assertEqual(res.status_code, 201)
         response = self.app.get(
             '/api/v1/interventions', content_type='application/json',
-             headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
+            headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
         self.assertEqual(response.status_code, 200)
+
+    def test_get_a_single_user_intervention(self):
+        token = self.create_token()
+        intervention_data = {
+        "comment": "Power outage in Mulago",
+        "images": "elec.jpeg",
+        "location": "Mulago",
+        "videos": "blackout.mp4"
+    }
+    res = self.app.post(
+        '/api/v1/interventions', content_type='application/json',
+        headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
+    self.assertEqual(res.status_code, 201)
+    response = self.app.get(
+        '/api/v1/user/interventions/1', content_type='application/json',
+        headers={'Authorization': f'Bearer {token}'}, data=json.dumps(intervention_data))
+    self.assertEqual(response.status_code, 200)
