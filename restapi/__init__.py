4f934cb79bca7f94523c992cdfbc5edd083b67c4
Jean Mark <kabohajeanmark@gmail.com>
2019-02-05 14:48:00 +0300

    Install and use flask_cors

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from restapi.views import redflag_views, user_views, intervention_views
 from flask_jwt_extended import jwt_required, JWTManager
 
 app = Flask(__name__, instance_relative_config=True)
+CORS(app)
 app.register_blueprint(intervention_views.BPrint)
 app.register_blueprint(user_views.BP)
 app.register_blueprint(redflag_views.bp)
