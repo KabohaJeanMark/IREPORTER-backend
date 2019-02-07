b62b0ffefe504e88c3f099dedd0096028641baf8
Jean Mark <kabohajeanmark@gmail.com>
2019-02-07 12:53:55 +0300

    Edit user functions

--- restapi/views/user_views.py
+++ restapi/views/user_views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from restapi.controllers.user_controllers import UserController
-from flask_jwt_extended import jwt_required
+
 
 BP = Blueprint("user_views", __name__, url_prefix="/api/v1")
 
@@ -15,7 +15,3 @@ def add_user():
 @BP.route("/auth/login", methods=["POST"])
 def sign_in():
     return myUser.login_user()
-
-@BP.route("/auth/admin", methods = ["POST"])
-def admin_sign_up():
-    return myUser.admin_login()
